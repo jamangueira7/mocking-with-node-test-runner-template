23faeaeaecf1c870904baa3418aef3dbfbf89e0b
João Mangueira <jamjs7@gmail.com>
2023-09-25 11:26:03 -0300

    add teste do create e imagem da cobertura de teste

--- src/todoService.js
+++ src/todoService.js
@@ -1,3 +1,5 @@
+import Todo from "./todo.js";
+
 export default class TodoService {
     #todoRepository
 
@@ -27,6 +29,6 @@ export default class TodoService {
 
     async list(query) {
         return (await this.#todoRepository.list())
-            .map(({ text, ...result }) => ({ text: text.toUpperCase(), ...result }))
+            .map(({ text, ...result }) => (new Todo({ text: text.toUpperCase(), ...result })))
     }
 }
--- test/todoService.test.js
+++ test/todoService.test.js
@@ -1,6 +1,7 @@
-import { describe, it, beforeEach, before, after } from 'node:test'
+import { describe, it, beforeEach, before, after, afterEach } from 'node:test'
 import assert from 'node:assert'
 import crypto from 'node:crypto'
+import sinon from 'sinon'
 import TodoService from "../src/todoService.js";
 import Todo from "../src/todo.js";
 
@@ -28,7 +29,7 @@ describe('todoService test Suite', () => {
         })
 
         it('should return a list of items with uppercase text', async () => {
-            const expected = mockDatabase.map(({ text, ...result }) => ({ text: text.toUpperCase(), ...result }))
+            const expected = mockDatabase.map(({ text, ...result }) => (new Todo({ text: text.toUpperCase(), ...result })))
             const result = await _todoService.list()
             assert.deepStrictEqual(result, expected)
             const fnMock = _dependencies.todoRepository.list.mock
@@ -39,23 +40,27 @@ describe('todoService test Suite', () => {
     describe('#create', () => {
         let _todoService
         let _dependencies
-        const DEFAULT_ID = '0001'
+        let _sandbox
         const mockDatabase = {
             text: 'I must fix my old car',
             when: new Date('2021-02-21T00:00:00.000Z'),
             status: 'late',
             id: 'f6106fee-6d5e-43f6-a9ba-2c8f222dd5d2'
         }
+        const DEFAULT_ID = mockDatabase.id
 
         //mockando o dado do ID para o teste não depender do ambiente
         before(async() => {
             crypto.randomUUID = () => DEFAULT_ID
+            _sandbox = sinon.createSandbox()
         })
 
         //trazendo o dado original caso algum outro teste precise
         after(async () => {
             crypto.randomUUID = (await import('node:crypto')).randomUUID
         })
+
+        afterEach(() => _sandbox.restore())
 
         beforeEach((context) => {
             _dependencies = {
@@ -85,6 +90,52 @@ describe('todoService test Suite', () => {
             }
             const result = await _todoService.create(input)
             assert.deepStrictEqual(JSON.stringify(result), JSON.stringify(expected))
+        })
+
+        it(`should save todo item with late status when the property is further than today`, async () => {
+            const input = new Todo({
+                text: 'I must fix my old car',
+                when: new Date('2020-02-21T00:00:00.000Z')
+            })
+            const expected = {
+                text: 'I must fix my old car',
+                when: new Date('2020-02-21T00:00:00.000Z'),
+                status: 'late',
+                id: DEFAULT_ID
+            }
+
+            const today = new Date('2020-02-22')
+            _sandbox.useFakeTimers(today.getTime())
+
+            await _todoService.create(input)
+
+            const fnMock = _dependencies.todoRepository.create.mock
+            assert.strictEqual(fnMock.callCount(), 1)
+
+            assert.deepStrictEqual(fnMock.calls[0].arguments[0], expected)
+        })
+
+        it(`should save todo item with pending status when the property is in the past`, async () => {
+            const input = new Todo({
+                text: 'I must fix my old car',
+                when: new Date('2020-02-22T00:00:00.000Z')
+            })
+            const expected = {
+                text: 'I must fix my old car',
+                when: new Date('2020-02-22T00:00:00.000Z'),
+                status: 'pending',
+                id: DEFAULT_ID
+            }
+
+            const today = new Date('2020-02-21')
+            _sandbox.useFakeTimers(today.getTime())
+
+            await _todoService.create(input)
+
+            const fnMock = _dependencies.todoRepository.create.mock
+            assert.strictEqual(fnMock.callCount(), 1)
+
+            assert.deepStrictEqual(fnMock.calls[0].arguments[0], expected)
         })
     })
 })
